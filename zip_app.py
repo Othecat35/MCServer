2d7b38aa385aab13c01a68683d39b143f5f2c9ae
Othecat35 <othecat35@gmail.com>
2026-05-27 20:33:58 +0700

    refactor: make code use zipapp

---
+++ zip_app.py
@@ -0,0 +1,13 @@
+import zipapp
+from pathlib import Path
+
+source_dir = Path("mcserver")
+
+target_dir = Path("dist")
+target_file = target_dir / "mcserver"
+
+target_dir.mkdir(exist_ok=True)
+
+print("Zipping app...")
+zipapp.create_archive(source_dir, target_file, "/usr/bin/python3")
+print("Done!")
