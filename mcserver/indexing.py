480103feb0552938d823471025cdaf8be3e1c303
Othecat35 <othecat35@gmail.com>
2026-06-12 23:51:04 +0700

    night commit

--- mcserver/indexing.py
+++ mcserver/indexing.py
@@ -5,24 +5,53 @@ import logging as log
 from shared import mcserver_dir
 
 # Variables
-project_indexes_dir = mcserver_dir / "project_indexes"
-slug_id_file = project_indexes_dir / ".slug_id.json"
+indexes_dir = mcserver_dir / "indexes"
+slug_id_file = indexes_dir / ".slug_id.json"
+
+# Directory structure reference:
+#.mcserver/
+#  indexes/
+#    .slug_id.json
+#    <project_id>/
+#      metadata.json
+#      relationship.json
+
 
 # Functions
-def project_index_exists(project_id: str) -> bool:
-  return (project_indexes_dir / f"{project_id}.json").exists()
+def crate_project_index(project_id: str, metadata: dict | None = None, relationship: dict | None = None):
+  project_index = indexes_dir / project_id
+  project_metadata = project_index / "metadata.json"
+  project_relationship = project_index / "relationship.json"
 
-def write_project_index(project_id: str, project_data: dict) -> None:
-  (project_indexes_dir / f"{project_id}.json").write_text(json.dumps(project_data, indent=2))
+  if project_index.exists():
+    raise FileExistsError(f"Project index for '{project_id}' already exists")
 
-def read_project_index(project_id: str) -> dict:
-  return json.loads((project_indexes_dir / f"{project_id}.json").read_text())
+  project_index.mkdir()
+  project_metadata.write_text(json.dumps(metadata, indent=2))
+  project_relationship.write_text(json.dumps(relationship, indent=2))
 
-def create_project_index(project_id: str, project_data: dict) -> None:
-  if project_index_exists(project_id):
-    raise FileExistsError(f"Project index file for '{project_id}' already exists")
-  else:
-    write_project_index(project_id, project_data)
+def read_project_index(project_id: str) -> dict[str, dict]:
+  project_index = indexes_dir / project_id
+  project_metadata = project_index / "metadata.json"
+  project_relationship = project_index / "relationship.json"
+
+  return {
+    "metadata": json.loads(project_metadata.read_text()),
+    "relationship": json.loads(project_relationship.read_text())
+  }
+
+def update_project_index(project_id: str, new_metadata: dict | None = None, new_relationship: dict | None = None):
+    project_index = indexes_dir / project_id
+  project_metadata = project_index / "metadata.json"
+  project_relationship = project_index / "relationship.json"
+
+def delete_project_index(project_id: str) -> None:
+  project_index = indexes_dir / project_id
+  project_metadata = project_index / "metadata.json"
+  project_relationship = project_index / "relationship.json"
+
+  project_metadata.unlink()
+  project_relationship.unlink()
 
 slug_id = {}
 def slug_to_id(project_slug: str) -> str:
