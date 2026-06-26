5b85e8839b11c74aeed47cfdeccdb93ffdf43a6b
Othecat35 <othecat35@gmail.com>
2026-06-26 15:16:41 +0700

    fic: is state.py fixed yet?

--- mcserver/state.py
+++ mcserver/state.py
@@ -2,6 +2,8 @@
 # Standard
 import json, os, sys
 
+import logging as log
+
 from pathlib import Path
 
 # MCServer
@@ -12,24 +14,30 @@ state_file = mcserver_dir / "state.json"
 
 # Functions
 def get_start_time(pid: int | str) -> int:
-  return int((Path("/proc") / str(pid) / "stat").read_text().split()[21])
+  proc_path = Path("/proc") / str(pid)
+  if not proc_path.exists():
+    log.debug(f"Process ID ({pid}) seems to not exist")
+    return 0
+
+  return int((proc_path / "stat").read_text().split()[21])
 
 def get_state() -> dict:
   return json.loads(state_file.read_text())
 
-def is_active():
+def is_active() -> bool:
   current_state = get_state()
   if not current_state:
     return False
-  elif current_state.get("start_time", 0) != get_start_time(current_state["process_id"]):
+  elif current_state["start_time"] == get_start_time(current_state["process_id"]):
     return True
 
   return False
 
-def set_state():
+def set_state(action: str) -> None:
   state_data = {
     "process_id": os.getpid(),
-    "start_time": get_start_time("self")
+    "start_time": get_start_time("self"),
+    "action": action
   }
 
   state_file.write_text(json.dumps(state_data, indent=2))
