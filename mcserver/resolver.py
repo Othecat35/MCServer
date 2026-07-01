947f1716afe648a75e2fcabaefe607917a8434be
Othecat35 <othecat35@gmail.com>
2026-07-02 00:09:17 +0700

    fix: almost finishing the resolver.py

--- mcserver/resolver.py
+++ mcserver/resolver.py
@@ -38,22 +38,32 @@ class NodeData(typing.TypedDict):
   dependants: dict[str, typing.Literal[0] | typing.Literal[1] | typing.Literal[2] | typing.Literal[3]]
 
 # Functions
-def get_dependecies() -> dict[str, typing.Literal[0] | typing.Literal[1] | typing.Literal[2] | typing.Literal[3]]:
+def filter_dependencies_type(dependencies: dict, filter_dependency_type: int) -> list[str]:
+  dependencies_list = []
+  for dependency_node, dependency_type in dependencies.items():
+    if dependency_type == filter_dependency_type:
+      dependencies_list.append(dependency_node)
+
+  return dependencies_list
+
+def get_dependency_data(node_id: str, game_versions: list[str] | str, loader_versions: list[str] | str) -> dict[str, typing.Literal[0] | typing.Literal[1] | typing.Literal[2] | typing.Literal[3]]:
+  if isinstance(game_versions, str): game_versions = [game_versions]
+  if isinstance(loader_versions, str): loader_versions = [loader_versions]
   return {}
 
-def is_conflicting(node_type: typing.Literal[0] | typing.Literal[1] | typing.Literal[2] | typing.Literal[3], dependant_type: typing.Literal[0] | typing.Literal[1] | typing.Literal[2] | typing.Literal[3]) -> bool:
-  if node_type == dependency_types["incompatible"] and dependant_type == dependency_types["required"]:
-    return True
-  elif node_type == dependency_types["required"] and dependant_type == dependency_types["incompatible"]:
-    return True
+def check_conflics(node_id: str, node_data: NodeData, dependant_id: str, dependant_type: typing.Literal[0] | typing.Literal[1] | typing.Literal[2] | typing.Literal[3]) -> None:
+  if node_data["type"] == dependency_types["required"] and dependant_type == dependency_types["incompatible"]:
+    raise DependencyConflictError(node_id, dependant_id, filter_dependencies_type(node_data["dependants"], dependency_types["required"]))
+  elif node_data["type"] == dependency_types["incompatible"] and dependant_type == dependency_types["required"]:
+    raise DependencyConflictError(node_id, filter_dependencies_type(node_data["dependants"], dependency_types["incompatible"]), dependant_id)
 
-  return False
-
-def resolve_dependencies(initial_seeds: list[str] | str, get_dependency_data: typing.Callable, is_conflicting: typing.Callable) -> dict[str, NodeData]:
+def resolve_dependencies(initial_seeds: list[str] | str, game_versions: list[str] | str, loader_versions: list[str] | str, get_dependency_data: typing.Callable, check_conflicts: typing.Callable) -> dict[str, NodeData]:
   if isinstance(initial_seeds, str): initial_seeds = [initial_seeds]
-  unresolved_nodes = collections.deque()
+  if isinstance(game_versions, str): game_versions = [game_versions]
+  if isinstance(loader_versions, str): loader_versions = [loader_versions]
 
-  resolved_nodes = set()
+  unresolved_nodes = collections.deque()
+  resolved_nodes: set[str] = set()
   resolved_data = {}
 
   for seed in initial_seeds:
@@ -71,21 +81,18 @@ def resolve_dependencies(initial_seeds: list[str] | str, get_dependency_data: ty
     if node_id in resolved_nodes:
       continue
 
-    dependencies = get_dependency_data(node_id)
+    dependencies = get_dependency_data(node_id, game_versions, loader_versions)
     resolved_data[node_id]["dependencies"] = dependencies
     for dependency_id, dependency_type in dependencies.items():
       if dependency_id in resolved_data:
-        dependency_node = resolved_data[dependency_id]
-        if is_conflicting(dependency_node["type"], dependency_type):
-          raise DependencyConflictError(dependency_node["dependencies"], {
-            node_id: dependency_type
-          })
+        dependency_data = resolved_data[dependency_id]
 
-        dependency_node["type"] = max(dependency_node["type"], dependency_type)
+        check_conflicts(dependency_id, dependency_data,  node_id, dependency_type)
+        dependency_data["type"] = max(dependency_data["type"], dependency_type)
         if dependency_type == dependency_types["required"]:
           unresolved_nodes.append(dependency_id)
 
-        dependency_node["dependants"][node_id] = dependency_type
+        dependency_data["dependants"][node_id] = dependency_type
       else:
         resolved_data[dependency_id] = {
           "manual": False,
@@ -102,8 +109,3 @@ def resolve_dependencies(initial_seeds: list[str] | str, get_dependency_data: ty
     resolved_nodes.add(node_id)
 
   return resolved_data
-
-
-
-
-
