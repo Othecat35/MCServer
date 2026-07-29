d2646e2167487daa8bab0cfa7b446f95ef8d29a9
Othecat35 <othecat35@gmail.com>
2026-07-29 20:35:40 +0700

    refactor: more lazy loading for my HDD user folks

--- src/mcserver/commands.py
+++ src/mcserver/commands.py
@@ -189,8 +189,10 @@ def show_projects(args: argparseNamespace) -> int:
     return 0
 
 def start_server(args: argparseNamespace) -> int:
-    # Check MCServer status
+    import logging as log
     from . import state
+
+    # Check MCServer status
     current_state = state.get_state()
     if current_state["is_active"]:
         log.error(f"There's another active MCServer process ({current_state["action"]}) running with process ID: {current_state["process_id"]}")
@@ -199,36 +201,41 @@ def start_server(args: argparseNamespace) -> int:
     # Set state
     state.set_state("running_server")
 
-    import json
     import os
     import shutil
-    import logging as log
     from pathlib import Path
 
     from . import config
-    from . import networking
 
     # Load configs
-    server_config = config.load_config("server")
     launcher_config = config.load_config("launcher")
-
-    game_version = server_config["game_version"]
-    server_loader = server_config["loader"]
-
-    loader_name = server_loader["name"]
-    loader_version = server_loader["version"]
 
     jarfile = Path(launcher_config["jarfile"])
     memory_config = launcher_config["ram"]
 
     # Download jarfile
     if not jarfile.exists():
+        import json
+        from . import networking
+
         state.set_state("downloading_server")
+
+        server_config = config.load_config("server")
+        game_version = server_config["game_version"]
+        server_loader = server_config["loader"]
+
+        loader_name = server_loader["name"]
+        loader_version = server_loader["version"]
+
+        if loader_name != "vanilla" and loader_version is None:
+            log.error(f"Loader version cannot be null")
+            return 1
 
         match loader_name:
             # Mod loaders
             case "fabric":
                 from .fabricmc import meta as fabricmc_meta
+
                 log.info(f"Downloading Fabric loader {loader_version} for Minecraft version {game_version}...")
                 networking.download(fabricmc_meta.download_url(game_version, loader_version, "1.0.1"), jarfile)
             
@@ -243,10 +250,12 @@ def start_server(args: argparseNamespace) -> int:
             case "paper":
                 from .papermc import api as papermc_api
                 log.info(f"Downloading Paper build {loader_version} for Minecraft version {game_version}...")
+
                 download_prop = papermc_api.get_project_build("paper", game_version, loader_version)["download_props"]["server:default"]
                 networking.download(download_prop["download_url"], jarfile)
             case "purpur":
                 from .purpurmc import api as purpurmc_api
+
                 log.info(f"Downloading Purpur build {loader_version} for Minecraft version {game_version}...")
                 networking.download(purpurmc_api.download_url("purpur", game_version, loader_version), jarfile)
 
@@ -254,6 +263,7 @@ def start_server(args: argparseNamespace) -> int:
             case "vanilla":
                 from .mojang import manifest as mojang_manifest
                 log.info(f"Downloading vanilla Minecraft version {game_version}...")
+
                 version_manifest = mojang_manifest.get_version_manifest()["game_versions"]
                 package_url = None
                 for version in version_manifest:
@@ -268,6 +278,7 @@ def start_server(args: argparseNamespace) -> int:
                 package_data = networking.request(package_url)["text"]
                 package_json = json.loads(package_data)
                 version_download = package_json["downloads"]["server"]
+
                 networking.download(version_download["url"], jarfile, hashes={
                     "sha1": version_download["sha1"]
                 })
