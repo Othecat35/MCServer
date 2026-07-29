1fd8b67a84f246e3b57b206a973cbb904cd23216
Othecat35 <othecat35@gmail.com>
2026-07-29 21:39:37 +0700

    feat: zip_app now ignores __pycache__

--- zip_app.py
+++ zip_app.py
@@ -1,18 +1,32 @@
+# Imports
 import zipapp
 from pathlib import Path
 
+# Paths
 source_dir = Path("src/")
-print(f"- Source      : '{source_dir}'")
 
 target_dir = Path("dist")
 target_dir.mkdir(exist_ok=True)
-
 target_file = target_dir / "mcserver"
-print(f"- Target      : '{target_file}'")
 
 python_interpreter = "/usr/bin/env python3"
-print(f"- Interpreter : '{python_interpreter}'")
 
+# Print Information
+print(f"""\
+Source      : '{source_dir}'
+Interpreter : '{python_interpreter}'
+Target      : '{target_file}'
+""")
+
+# Filter function
+def filter_file(file: Path) -> bool:
+    if file.name in ["__pycache__"]:
+        print(f"Ignoring path: '{source_dir}/{file}'")
+        return False
+
+    return True
+
+# Zip the App
 print("Creating archive...")
-zipapp.create_archive(source=source_dir, target=target_file, interpreter=python_interpreter)
-print("Done!")
+zipapp.create_archive(source=source_dir, target=target_file, interpreter=python_interpreter, filter=filter_file)
+print("Zipping complete.")
