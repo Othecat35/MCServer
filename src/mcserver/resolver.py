5199c3af784b8fc4e78c28c825b1fab4044ff63e
Othecat35 <othecat35@gmail.com>
2026-08-09 21:40:58 +0700

    chore: why I even do this?

--- src/mcserver/resolver.py
+++ src/mcserver/resolver.py
@@ -1,37 +1,82 @@
 import logging as log
 from collections import deque
 from collections.abc import Callable
+from typing import TypedDict
 
-def resolve_dependencies(initial_nodes: list[str] | str, get_dependencies: Callable, check_conflict: Callable) -> list:
-    if isinstance(initial_nodes, str):
-        initial_nodes = [initial_nodes]
+class Dependency(TypedDict):
+    project_id: str
+    dependency_type: str
 
-    queued_nodes: deque[str] = deque()
-    visited_nodes: set[str] = set()
-    dependency_graph: dict[str, dict] = {}
+class Project(TypedDict):
+    is_manual: bool
+    dependencies: dict[str, int]
+    dependents: dict[str, int]
 
-    for node in initial_nodes:
-        log.debug(f"Adding node '{project_id}' to queued nodes")
-        queued_nodes.append(node_id)
-        node_data = {
+class ProjectData(TypedDict):
+    project_id: str
+    is_manual: bool
+    dependencies: list[Dependency]
+    dependents: list[Dependency]
+
+dependency_type = {
+    "embedded": 0,
+    "optional": 1,
+    "required": 2,
+    "incompatible": 3
+}
+
+def test_dependencies(project_id: str) -> dict[str, int]:
+    dependencies = {
+        "a": {
+            "b": 2,
+            "c": 2,
+            "d": 2
+        },
+        "b": {
+            "c": 2,
+            "d": 2
+        },
+        "c": {
+            "e": 2
+        },
+        "d": {
+            "e": 2
+        },
+        "e": {}
+    }
+
+    return dependencies[project_id]
+
+def resolve_dependencies(project_ids: list[str] | str, get_dependencies: Callable) -> list:
+    if isinstance(project_ids, str):
+        project_ids = [project_ids]
+
+    queued_projects: deque[str] = deque()
+    visited_projects: set[str] = set()
+    dependency_graph: dict[str, Project] = {}
+
+    for project_id in project_ids:
+        log.debug(f"Adding project '{project_id}' to queue")
+        queued_projects.append(project_id)
+        project: Project = {
             "is_manual": True,
             "dependencies": {},
             "dependents": {}
         }
 
-        dependency_graph[project_id] = node_data
+        dependency_graph[project_id] = project
 
-    while queued_nodes:
-        project_id = queued_nodes.popleft()
-        if project_id in visited_nodes:
-            log.debug(f"Project ID '{project_id}' has already been visited")
+    while queued_projects:
+        project_id = queued_projects.popleft()
+        if project_id in visited_projects:
+            log.debug(f"Project '{project_id}' has already been visited")
             continue
 
         dependencies = get_dependencies(project_id)
         dependency_graph[project_id]["dependencies"] = dependencies
 
         for dependency_id, dependency_type in dependencies.items():
-            if dependency_id in visited_nodes:
+            if dependency_id in visited_projects:
                 dependency_graph[dependency_id]["dependents"][project_id] = dependency_type
             else:
                 dependency_graph[dependency_id] = {
@@ -42,8 +87,39 @@ def resolve_dependencies(initial_nodes: list[str] | str, get_dependencies: Calla
                     }
                 }
 
-                if dependency_type == "required":
-                    log.debug(f"Adding dependency ID '{dependency_id}' to queued nodes")
-                    queued_nodes.append(dependency_id)
+                if dependency_type == 2: # NOTE: yay hardcoded value until I need to change this
+                    log.debug(f"Adding dependency '{dependency_id}' to queue")
+                    queued_projects.append(dependency_id)
 
-        visited_nodes.add(project_id)
+        visited_projects.add(project_id)
+
+    resolved_dependencies: list[ProjectData] = []
+    for project_id, project in dependency_graph.items():
+        dependencies: list[Dependency] = []
+        for dependency_id, dependency_type in project["dependencies"]:
+            dependency: Dependency = {
+                "project_id": dependency_id,
+                "dependency_type": dependency_type
+            }
+
+            dependencies.append(dependency)
+
+        dependents: list[Dependency] = {}
+        for dependency_id, dependency_type in project["dependencies"]:
+            dependency: Dependency = {
+                "project_id": dependency_id,
+                "dependency_type": dependency_type
+            }
+
+
+        project_data: ProjectData = {
+            "project_id": project_id,
+            "is_manual": project["is_manual"],
+            "dependencies": dependencies
+        }
+
+        resolved_dependencies.append(project_data)
+
+    return resolved_dependencies
+
+print(resolve_dependencies("a", test_dependencies))
