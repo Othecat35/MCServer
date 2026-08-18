e18ef3f7c9af22568fa756e7823fb7f0b2f7e01d
Othecat35 <othecat35@gmail.com>
2026-08-18 08:56:54 +0700

    fix: change resolver test initial dependency

--- src/mcserver/resolver.py
+++ src/mcserver/resolver.py
@@ -161,4 +161,4 @@ def test_dependencies(project_id: str) -> dict[str, int]:
     return human_to_resolver(dependencies[project_id])
 
 
-print(resolver_to_human(resolve_dependencies("a", test_dependencies, required_only)))
+print(resolver_to_human(resolve_dependencies("thdilos-fox-origin-expanded", test_dependencies, required_only)))
