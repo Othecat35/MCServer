eaa7bf55e35d0b46bd241b72f1220cd34024dd79
Othecat35 <othecat35@gmail.com>
2026-09-10 17:47:18 +0700

    fix: properties parser to be more accurate

--- src/mcserver/server_files/properties.py
+++ src/mcserver/server_files/properties.py
@@ -18,24 +18,24 @@ def loads(properties_data: str) -> ParsedProperties:
             continue
 
         if line.startswith("#"):
-            parsed_properties["comments"].append(line)
+            parsed_properties["comments"].append(line.strip()[1:])
 
         if not "=" in line:
             continue
 
         key, value = line.split("=", 1)
-        parsed_properties[key.strip()] = value
+        parsed_properties["data"][key.strip()] = value.replace("\:", r":")
 
     return parsed_properties
 
 
-# Dump only support comments that are oon top of the file
+# Dump only support comments that are on top of the file
 def dumps(parsed_properties: ParsedProperties) -> str:
-    lines: list[str] = []
+    properties_data: list[str] = []
     for comment in parsed_properties["comments"]:
         lines.append(f"#{comment}")
 
     for key, value in parsed_properties["data"].items():
-        lines.append(f"{key}={value}")
+        lines.append(f"{key}={value.replace(':', r'\:')}")
 
     return "\n".join(lines)
